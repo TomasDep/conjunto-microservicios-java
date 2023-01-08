0cc3b333ee82863403ddf7ba2c22f0741b9acb3f
TomasDep <tomas.valenzuelacb@gmail.com>
2023-01-08 17:21:13 -0300

    Se implementas interfaces FeingClient

---
+++ msvc-cursos/src/main/java/org/tomdep/springcloud/msvc/cursos/clients/UsuarioClientRest.java
@@ -0,0 +1,19 @@
+package org.tomdep.springcloud.msvc.cursos.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+import org.tomdep.springcloud.msvc.cursos.models.Usuario;
+
+import java.util.List;
+
+@FeignClient(name = "msvc-usuarios", url = "localhost:8001/api/v1/usuarios")
+public interface UsuarioClientRest {
+    @GetMapping("/{id}")
+    Usuario detalle(@PathVariable Long id);
+
+    @GetMapping("/por/curso")
+    List<Usuario> obtenerAlumnosPorCurso(@RequestParam Iterable<Long> ids);
+    @PostMapping
+    Usuario crear(@RequestBody Usuario usuario);
+
+}
