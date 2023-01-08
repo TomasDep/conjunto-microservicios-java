5de5a8e5bed6c174f390badb74a2a763afab07c8
TomasDep <tomas.valenzuelacb@gmail.com>
2023-01-08 17:20:19 -0300

    Se habilita feign client

--- msvc-usuarios/src/main/java/org/tomdep/springcloud/msvc/usuarios/MsvcUsuariosApplication.java
+++ msvc-usuarios/src/main/java/org/tomdep/springcloud/msvc/usuarios/MsvcUsuariosApplication.java
@@ -2,8 +2,10 @@ package org.tomdep.springcloud.msvc.usuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MsvcUsuariosApplication {
 
 	public static void main(String[] args) {
